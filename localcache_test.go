358802538b4ac2ed5a05a93c141bae5aad144272
ivan.ho <ivan.ho@elevatelimited.com>
2021-12-14 18:01:14 +0800

    misc: remove no use comment

--- localcache_test.go
+++ localcache_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestSet(t *testing.T) {
-	//c := &localcache{}
 	Set("key1", 100)
 	got := Data["key1"].content
 	expect := 100
@@ -29,7 +28,6 @@ func TestGet(t *testing.T) {
 }
 
 func TestGetWithExpiredData(t *testing.T) {
-	//c := &localcache{}
 	Data["key3"] = CacheData{content: 200, expiredTime: time.Now().AddDate(0, -1, 0)}
 	expectError := ErrDataExpired
 	_, err := Get("key3")
